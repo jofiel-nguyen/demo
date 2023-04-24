cf58109e972e250f980c6280e58786e1560e8cb7
wren <zennguyen2511@gmail.com>
2023-04-24 18:42:50 -0500

    creating new files

---
+++ index.js
@@ -0,0 +1,33 @@
+const readline = require('readline');
+const fs = require('fs');
+const { createSVG } = require('./svgUtils');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const MAX_TEXT_LENGTH = 3;
+const SHAPE_CHOICES = ['circle', 'triangle', 'square'];
+
+function prompt(question) {
+  return new Promise(resolve => {
+    rl.question(question, resolve);
+  });
+}
+
+async function main() {
+  const text = await prompt(`Enter up to ${MAX_TEXT_LENGTH} characters: `);
+  const color = await prompt('Enter text color (keyword or hexadecimal): ');
+  const shape = await prompt(`Choose a shape (${SHAPE_CHOICES.join(', ')}): `);
+  const shapeColor = await prompt('Enter shape color (keyword or hexadecimal): ');
+
+  // Create SVG file based on user input
+  const svgData = createSVG(text, color, shape, shapeColor);
+  fs.writeFileSync('logo.svg', svgData);
+
+  console.log('Generated logo.svg');
+  rl.close();
+}
+
+main().catch(err => console.error(err));
